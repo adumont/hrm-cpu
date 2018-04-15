6f47757b3141aae62629d0078b6a32d88a739eac
Alexandre Dumont <adumont@gmail.com>
2018-04-15 15:04:50 +0200

    Add verilog program module

---
+++ verilog/program.v
@@ -0,0 +1,31 @@
+module program (
+        // Input: Addr
+
+        input wire [7:0] Addr,
+        input clk,
+
+        // Output: Data, 8 bit: Instruction or Operand
+
+        output wire [7:0] Data
+        // output wire signed [11:0] Data   # do we need to have "signed" here?
+    );
+
+    // signals are coded into the microcode.rom file
+    parameter PROGRAM = "program.rom";
+    parameter SIZE = 256;
+
+    reg [7:0] rom [0: SIZE-1 ];
+
+    initial begin
+        $readmemh(PROGRAM, rom);
+    end
+
+    reg [7:0] r_data;
+
+    always @(posedge clk) begin
+        r_data <= rom[Addr];
+    end
+
+    assign Data = r_data;
+
+endmodule
---
+++ verilog/program_tb.v
@@ -0,0 +1,35 @@
+module program_tb();
+
+	reg [7:0] Addr;
+
+	wire [7:0] Data;
+
+	localparam SIZE=10;
+
+	// Instanciate DUT
+	program program0 (.Addr(Addr), .clk(clk), .Data(Data));
+	defparam program0.PROGRAM = "program.rom";
+	defparam program0.SIZE = SIZE;
+
+	// Simulate clock
+	reg clk  = 0;
+    always #1 clk = ~clk;
+
+	// start simulation
+	initial begin
+		$dumpfile("program_tb.vcd");
+		$dumpvars(0, program_tb);
+
+		#2;
+
+        for (Addr=0; Addr<SIZE-1; Addr=Addr+1)
+        begin
+            #2;
+        end
+
+		# 10
+		$finish;
+
+	end
+
+endmodule
