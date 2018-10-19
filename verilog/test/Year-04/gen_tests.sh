7cc66bdd3a2206cb968e1b1e2904d5b89214332d
Alexandre Dumont <adumont@gmail.com>
2018-10-19 23:13:10 +0200

    Add test generator for Year-04, and 20 new tests

---
+++ verilog/test/Year-04/gen_tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+TEST_NUMBER=20
+
+TIMING=$( mktemp )
+DATA=$( mktemp )
+
+for t in $( seq -w 0 $(( TEST_NUMBER -1 )) )
+do
+  echo $t
+  TESTIN=test$t.in
+  TESTOUT=test$t.out
+
+  # between 10..49 numbers in input
+  SIZE=$( cat /dev/urandom | tr -dc '0-9' | fold -w 2 | grep ^[1-4] | grep -v 00 | head -1 )
+
+  # timing, between 01..59
+  cat /dev/urandom | tr -dc '0-9' | fold -w 2 | grep -v 00 | grep ^[0-5] | head -n $SIZE > $TIMING
+
+  # datos
+  cat /dev/urandom | tr -dc '0-9a-f' | fold -w 2 | head -n $SIZE > $DATA
+
+  paste -d " " $TIMING $DATA > $TESTIN
+
+  i=0
+  cat $DATA | while read IN
+  do
+    if [ $(( i % 2 )) -eq 0 ]
+    then
+      OLD=$IN
+    else
+      echo $IN
+      echo $OLD
+    fi
+    i=$(( i + 1 ))
+  done > $TESTOUT
+
+done
