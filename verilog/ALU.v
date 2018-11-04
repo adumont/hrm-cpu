83031692420ed81e41c2894f31194edc08adacf7
Alexandre Dumont <adumont@gmail.com>
2018-11-04 17:00:56 +0100

    ALU: Fix delayed assignments (<=) in non-clocked block

--- verilog/ALU.v
+++ verilog/ALU.v
@@ -23,19 +23,19 @@ module ALU (
     // combination logic for: aluOut
     always @(*)
     case (aluCtl[1:0])
-        2'b 00: aluOut <= inR + inM;
-        2'b 01: aluOut <= inR - inM;
-        2'b 10: aluOut <= inM + 8'b1;
-        default: aluOut <= inM - 8'b1; // 2'b 11
+        2'b 00: aluOut = inR + inM;
+        2'b 01: aluOut = inR - inM;
+        2'b 10: aluOut = inM + 8'b1;
+        default: aluOut = inM - 8'b1; // 2'b 11
     endcase
 
     // combination logic for: flag
     always @(*)
     begin
         if( aluCtl[2] )
-            flag <=  inR[7] ; // R  < 0 ?
+            flag =  inR[7] ; // R  < 0 ?
         else
-            flag <= ( inR == 8'b 0 ); // R == 0 ?
+            flag = ( inR == 8'b 0 ); // R == 0 ?
     end
 
 endmodule
