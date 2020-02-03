3c3605b1c3fb65e8ca1d17a79838eb5090306370
Alexandre Dumont <adumont@gmail.com>
2020-02-03 23:10:51 +0100

    ram.v: use Single Port RAM

--- verilog/ram.v
+++ verilog/ram.v
@@ -1,38 +1,31 @@
 `default_nettype none
 
+// ** Single-Port RAM **
+// LATTICE, Memory Usage Guide for iCE40 Devices
+// Technical Note TN1250, June 2016
+// Appendix A. Standard HDL Code References, p20
 module ram #(
-        parameter data_width =  8,
-        parameter ram_size =  2,
-        parameter addr_width =  $clog2(ram_size)
-    ) (
-        input wire rclk,
-        input wire [addr_width-1:0] raddr,
-        output reg [data_width-1:0] dout,
-        input wire wclk,
-        input wire write_en,
-        input wire [addr_width-1:0] waddr,
-        input wire [data_width-1:0] din
-    );
+    parameter addr_width =  8,
+    parameter data_width =  8,
+    parameter ROMFILE = ""
+  ) (
+    input wire clk,
+    input wire [addr_width-1:0] addr,
+    input wire write_en,
+    input wire [data_width-1:0] din,
+    output reg [data_width-1:0] dout
+  );
 
-    reg [data_width-1:0] mem [ram_size-1:0];
+  reg [data_width-1:0] mem [(1<<addr_width)-1:0];
 
-    parameter ROMFILE = "";
-    initial begin
-        if( ROMFILE !=0 ) $readmemh(ROMFILE, mem);
-        dout=0;
-    end
+  initial begin
+      if( ROMFILE !=0 ) $readmemh(ROMFILE, mem);
+  end
 
-    always @(posedge rclk) // Read memory
-    begin
-        if (write_en)
-            dout <= din;
-        else
-            dout <= mem[raddr];
-    end
-
-    always @(posedge wclk) // Write memory.
-    begin
-        if (write_en) mem[waddr] <= din; // Using write address bus.
-    end
-
+  always @(posedge clk)
+  begin
+    if (write_en)
+      mem[(addr)] <= din;
+    dout = mem[addr];
+  end
 endmodule
