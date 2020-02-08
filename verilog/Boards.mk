bd798505ad99967af56c5a6544cf1d37e4d4da67
Alexandre Dumont <adumont@gmail.com>
2020-02-08 22:59:50 +0100

    Revert back to using built toolchain (no docker)
    Issue with docker on WSL (Windows Subsystem for Linux)
    
    This reverts commits:
    * 47ffc9b Add --package to nextpnr call
    * 181a28d Don't build yosys and arachne-pnr anymore
    * 09ff362 Build with dockerized yosys and nextpnr

--- verilog/Boards.mk
+++ verilog/Boards.mk
@@ -3,12 +3,10 @@ BOARD?=alhambra
 ifeq ($(BOARD), alhambra)
 
   PNRDEV:=1k
-  PNRPACK:=tq144
   M4_OPTIONS += -D_BOARD_HAVE_BUTTONS_
 
 else ifeq ($(BOARD), ice40hx8k)
 
   PNRDEV:=8k
-  PNRPACK:=ct256
 
 endif
